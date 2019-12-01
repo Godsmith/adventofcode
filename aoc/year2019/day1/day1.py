ed33497d4ad4998e907cde419d73ac069b2cdadb
Godsmith <filip.lange@gmail.com>
2019-12-01 21:06:55 +0100

    Refactoring day1

--- aoc/year2019/day1/day1.py
+++ aoc/year2019/day1/day1.py
@@ -3,23 +3,14 @@ from aoc.utils import ints
 
 
 def fuel(mass: int) -> int:
-    fuel_req = mass // 3 - 2
-    if fuel_req < 0:
-        return 0
-    return fuel_req
+    return mass // 3 - 2
 
 
 def total_fuel(mass: int) -> int:
-    out = 0
-    last_mass = mass
-    while True:
-        fuel_for_last_mass = fuel(last_mass)
-        out += fuel_for_last_mass
-        last_mass = fuel_for_last_mass
-        if fuel_for_last_mass == 0:
-            break
-
-    return out
+    fuel_req = mass // 3 - 2
+    if fuel_req < 0:
+        return 0
+    return fuel_req + total_fuel(fuel_req)
 
 
 # part 1
