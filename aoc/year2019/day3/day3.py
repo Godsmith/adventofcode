a1da22fc277a6cd0ef7e6c428c84371c21bdd63e
Godsmith <filip.lange@gmail.com>
2019-12-03 08:12:10 +0100

    day3 part 2

--- aoc/year2019/day3/day3.py
+++ aoc/year2019/day3/day3.py
@@ -1,4 +1,4 @@
-from typing import List, Set, Tuple
+from typing import List, Set, Tuple, Dict
 
 from aocd import data
 
@@ -8,13 +8,22 @@ DIRECTIONS = {'U': (0, 1), 'D': (0, -1), 'L': (-1, 0), 'R': (1, 0)}
 
 
 def main():
-#     data = """R75,D30,R83,U83,L12,D49,R71,U7,L72
-# U62,R66,U55,R34,D71,R55,D58,R83"""
-#     data="""R8,U5,L5,D3
-# U7,R6,D4,L4"""
+#     data = """r75,d30,r83,u83,l12,d49,r71,u7,l72
+# u62,r66,u55,r34,d71,r55,d58,r83"""
+    #     data="""R8,U5,L5,D3
+    # U7,R6,D4,L4"""
     wires = csv_rows(data)
     common_positions = positions(wires[0]) & positions(wires[1])
-    print(min(abs(position[0]) + abs(position[1]) for position in common_positions))
+    print(min(
+        abs(position[0]) + abs(position[1]) for position in common_positions))
+
+    delays0 = delays(wires[0])
+    delays1 = delays(wires[1])
+    total_delays_of_common_positions = [delays0[position] + delays1[position]
+                                        for position in common_positions]
+
+    print(min(total_delays_of_common_positions))
+
 
 def positions(wire: List[str]) -> Set[Tuple]:
     out = set()
@@ -25,6 +34,18 @@ def positions(wire: List[str]) -> Set[Tuple]:
             out.add(position)
     return out
 
+
+def delays(wire: List[str]) -> Dict[Tuple, int]:
+    out = {}
+    position = (0, 0)
+    delay = 0
+    for instruction in wire:
+        for _ in range(count(instruction)):
+            delay += 1
+            position = add(position, direction(instruction))
+            if position not in out:
+                out[position] = delay
+    return out
 
 
 def count(instruction):
