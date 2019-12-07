3e9bc0096e17672ec421d8a9f0c8bceb9c805afe
Filip Lange <filip.lange@gmail.com>
2019-12-07 18:01:37 +0100

    Refactoring

--- aoc/year2019/test_intcode.py
+++ aoc/year2019/test_intcode.py
@@ -18,27 +18,32 @@ def test_immediate_mode():
     i = Intcode([1002, 4, 3, 4, 33])
     assert i.run()[:] == [1002, 4, 3, 4, 99]
 
+
 def test_jump_if_false():
-    i = Intcode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])
+    i = Intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
     assert i.run(0).output == 0
-    i = Intcode([3,12,6,12,15,1,13,14,13,4,13,99,-1,0,1,9])
+    i = Intcode([3, 12, 6, 12, 15, 1, 13, 14, 13, 4, 13, 99, -1, 0, 1, 9])
     assert i.run(2).output == 1
 
+
 def test_jump_if_true():
-    i = Intcode([3,3,1105,-1,9,1101,0,0,12,4,12,99,1])
+    i = Intcode([3, 3, 1105, -1, 9, 1101, 0, 0, 12, 4, 12, 99, 1])
     assert i.run(0).output == 0
 
+
 def test_less_than():
-    i = Intcode([3,9,7,9,10,9,4,9,99,-1,8])
+    i = Intcode([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8])
     assert i.run(7).output == 1
-    i = Intcode([3,9,7,9,10,9,4,9,99,-1,8])
+    i = Intcode([3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8])
     assert i.run(8).output == 0
 
+
 def test_equals():
-    i = Intcode([3,3,1108,-1,8,3,4,3,99])
+    i = Intcode([3, 3, 1108, -1, 8, 3, 4, 3, 99])
     assert i.run(8).output == 1
-    i = Intcode([3,3,1108,-1,8,3,4,3,99])
+    i = Intcode([3, 3, 1108, -1, 8, 3, 4, 3, 99])
     assert i.run(7).output == 0
+
 
 class TestInstruction:
     def test_mode_of_parameter(self):
