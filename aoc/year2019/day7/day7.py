9bcaf0b54e11d4b5bb88d5a0f069e0f6ec362ef9
Filip Lange <filip.lange@gmail.com>
2019-12-07 18:56:50 +0100

    day7 part1

--- aoc/year2019/day7/day7.py
+++ aoc/year2019/day7/day7.py
@@ -1,5 +1,8 @@
-from aoc.year2019.intcode import Intcode
+from aocd import data
+from itertools import permutations
+
 from aoc.utils import ints
+from aoc.year2019.intcode import Intcode
 
 def output(program, phase_settings):
     intcode = Intcode(program)
@@ -8,9 +11,16 @@ def output(program, phase_settings):
         signal = intcode.run([phase_setting, signal]).output
     return signal
 
+def find_max_output(program):
+    max_output = 0
+    all_phase_settings = permutations((0,1,2,3,4))
+    for phase_settings in all_phase_settings:
+        max_output = max(max_output, output(program, phase_settings))
+    return max_output
+
 
 def main():
-    pass
+    print(find_max_output(ints(data)))
 
 
 if __name__ == '__main__':
