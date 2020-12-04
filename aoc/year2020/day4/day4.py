6d1db5256ca9096b55c0092f6fcd027d114e4a2f
Filip Lange <filip.lange@gmail.com>
2020-12-04 18:39:21 +0100

    Fewer LOC 2020 day4

--- aoc/year2020/day4/day4.py
+++ aoc/year2020/day4/day4.py
@@ -30,29 +30,22 @@ def is_valid2(passport):
                ('hcl', '^#[0-9a-f]{6}$'),
                ('ecl', '^(amb|blu|brn|gry|grn|hzl|oth)$'),
                ('pid', '^\d{9}$'))
-    for field, pattern in regexes:
-        try:
-            if not re.match(pattern, passport[field]):
-                return False
-        except KeyError:
-            return False
-    if not 1920 <= int(passport['byr']) <= 2002:
+    try:
+        results = [re.match(pattern, passport[field]) is not None for field, pattern in
+                   regexes]
+        results.append(1920 <= int(passport['byr']) <= 2002)
+        results.append(2010 <= int(passport['iyr']) <= 2020)
+        results.append(2020 <= int(passport['eyr']) <= 2030)
+        if 'cm' in passport['hgt']:
+            min_hgt = 150
+            max_hgt = 193
+        else:
+            min_hgt = 59
+            max_hgt = 76
+        results.append(min_hgt <= int(passport['hgt'][:-2]) <= max_hgt)
+    except (KeyError, ValueError):
         return False
-    if not 2010 <= int(passport['iyr']) <= 2020:
-        return False
-    if not 2020 <= int(passport['eyr']) <= 2030:
-        return False
-    if 'cm' in passport['hgt']:
-        min_hgt = 150
-        max_hgt = 193
-    else:
-        min_hgt = 59
-        max_hgt = 76
-    if not min_hgt <= int(passport['hgt'][:-2]) <= max_hgt:
-        return False
-    return True
-
+    return all(results)
 
 print(sum(map(is_valid, passports())))
-
 print(sum(map(is_valid2, passports())))
