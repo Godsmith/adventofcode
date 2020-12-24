0bc1bf56152f5f3bfe8c7eb25e115a2cd9481fa1
Filip Lange <filip.lange@gmail.com>
2020-12-24 08:23:06 +0100

    2020 day23

--- aoc/year2020/day23/day23.py
+++ aoc/year2020/day23/day23.py
@@ -2,6 +2,15 @@ from collections import deque
 
 from aocd import data
 
+
+def get_cups(ints):
+    cups = {}
+    for cup, cup_after in zip(ints, ints[1:]):
+        cups[cup] = cup_after
+    cups[ints[-1]] = ints[0]
+    return cups
+
+
 def move(cups, moves):
     current_cup = list(cups.keys())[0]
     for i in range(moves):
@@ -12,40 +21,37 @@ def move(cups, moves):
             cup_to_pick_up = cups[cup_to_pick_up]
         cups[current_cup] = cup_to_pick_up
         destination_number = current_cup - 1
-        if destination_number == 0:
-            destination_number = len(cups)
         while destination_number in picked_up:
             destination_number -= 1
-            if destination_number == 0:
-                destination_number = len(cups)
+        if destination_number == 0:
+            destination_number = len(cups)
         cups[picked_up[2]] = cups[destination_number]
         cups[destination_number] = picked_up[0]
         current_cup = cups[current_cup]
+
 
 def print_all_after_one(cups):
     cup = cups[1]
     for _ in range(len(cups) - 1):
         print(cup, end="")
         cup = cups[cup]
+    print()
 
 
-cups = {}
-for cup, cup_after in zip(data, data[1:]):
-    cups[int(cup)] = int(cup_after)
-cups[int(data[-1])] = int(data[0])
+# Part 1
 
+ints = list(map(int, data))
+
+cups = get_cups(ints)
 move(cups, 100)
 
 print_all_after_one(cups)
 
-cups = {}
-for cup, cup_after in zip(data, data[1:]):
-    cups[int(cup)] = int(cup_after)
-cups[int(data[-1])] = 10
-for i, j in zip(range(10, 1000001), range(11, 1000002)):
-    cups[i] = j
-cups[1000000] = int(data[0])
-#print(cups[7])
+# Part 2
+
+ints2 = list(map(int, data))
+ints2.extend(range(10, 1000001))
+cups = get_cups(ints2)
 
 move(cups, 10000000)
 
