f31b9c69227f99e6b9bdd429fbc8ae95a2433d97
Filip Lange <filip.lange@gmail.com>
2021-12-01 19:34:01 +0100

    Refactoring

--- aoc/year2021/day1/day1.py
+++ aoc/year2021/day1/day1.py
@@ -1,23 +1,20 @@
+from typing import List
+
 from aocd import data
 from aoc.utils import ints
 from more_itertools import sliding_window
 
 
-def deeper_count(depths):
-    count = 0
-    for i, depth in enumerate(depths):
-        if i == 0:
-            continue
-        if depth > depths[i - 1]:
-            count += 1
-    return count
+def deeper_count(depths: List[int]):
+    pairwise_depths = sliding_window(depths, 2)
+    depth_deltas = map(lambda depths: depths[1] - depths[0], pairwise_depths)
+    return len(list(filter(lambda depth_delta: depth_delta > 0, depth_deltas)))
 
-
+# Part 1
 depths = ints(data)
-
 print(deeper_count(depths))
 
+# Part 2
 groups_of_three_depths = sliding_window(depths, 3)
 averaged_depths = list(map(sum, groups_of_three_depths))
-
 print(deeper_count(averaged_depths))
