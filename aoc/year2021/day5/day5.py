90d049b8670bc3a3ea2da1c53e251b0bc4f8a2b4
Filip Lange <filip.lange@gmail.com>
2021-12-05 20:22:02 +0100

    Refactoring

--- aoc/year2021/day5/day5.py
+++ aoc/year2021/day5/day5.py
@@ -5,37 +5,22 @@ from aoc.utils import rows
 from more_itertools import flatten
 
 
+def to_coordinates(line, diagonals: bool):
+    x1, y1, x2, y2 = [int(number) for point in line.split(" -> ") for number in point.split(",")]
+    if x1 != x2 and y1 != y2 and not diagonals:
+        return []
+    dx = 0 if x1 == x2 else (1 if x2 > x1 else -1)
+    dy = 0 if y1 == y2 else (1 if y2 > y1 else -1)
+    xs = range(x1, x2 + dx, dx) if x1 != x2 else [x1] * (abs(y2 - y1) + 1)
+    ys = range(y1, y2 + dy, dy) if y1 != y2 else [y1] * (abs(x2 - x1) + 1)
+    return zip(xs, ys)
 
-def to_tuples(line, diagonals):
-    point1_string, point2_string = line.split(" -> ")
-    x1, y1 = [int(x) for x in point1_string.split(",")]
-    x2, y2 = [int(x) for x in point2_string.split(",")]
-    if x1 != x2 and y1 != y2:
-        if diagonals:
-            dx = (x2 - x1) / abs(x2-x1)
-            dy = (y2 - y1) / abs(y2-y1)
-            x, y = (x1, y1)
-            return_value = [(x, y)]
-            while True:
-                x += dx
-                y += dy
-                return_value.append((x, y))
-                if x == x2:
-                    return return_value
-        else:
-            return []
-    x1, x2 = sorted((x1, x2))
-    y1, y2 = sorted((y1, y2))
-    return [(x,y) for x in range(x1, x2 + 1) for y in range(y1, y2 + 1)]
 
 def overlaps(data, diagonals):
-    lines = rows(data)
-    tuples = list(flatten(to_tuples(line, diagonals) for line in lines))
-    c = Counter(tuples)
-    a = [count for t, count in c.most_common() if count > 1]
-    return len(a)
+    coordinates = flatten(to_coordinates(line, diagonals) for line in rows(data))
+    return sum(1 for _, count in Counter(coordinates).most_common() if count > 1)
+
 
 if __name__ == '__main__':
     print(overlaps(data, diagonals=False))
     print(overlaps(data, diagonals=True))
-
