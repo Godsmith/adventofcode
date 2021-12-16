8a1890b03d5929700989c19d492e01868767e487
Filip Lange <filip.lange@gmail.com>
2021-12-16 23:01:34 +0100

    2021 day16 part1 after refactoring, operator length type 0 still not working

--- aoc/year2021/day16/day16.py
+++ aoc/year2021/day16/day16.py
@@ -1,36 +1,56 @@
 from collections import deque
 
+
 class Packet:
+    def __init__(self, version, type_):
+        self.version = version
+        self.type = type_
+        self.literal_value = None  # type: Optional[int]
+        self.subpackets = []
+
+
+class PacketFactory:
     def __init__(self, hexadecimal_string: str):
         integer = int(hexadecimal_string, 16)
-        self.binary_string = format(integer, "b")
+        number_of_bits = len(hexadecimal_string) * 4
+        self.binary_string = format(integer, f"0{number_of_bits}b")
 
-        self.version = int(self.take_as_int(3))
-        self.type = int(self.take_as_int(3))
-
-        self.literal_value = None  # type: Optional[int]
-
+    def create_packets_until_end(self):
+        packets = []
         while self.binary_string != "0" * len(self.binary_string):
-            if self.type == 4:
-                last_group = False
-                literal_value_binary_string = ""
-                while not last_group:
-                    if self.take_as_string(1) == "0":
-                        last_group = True
-                    literal_value_binary_string += self.take_as_string(4)
-                self.literal_value = int(literal_value_binary_string, 2)
+            packets.append(self.create_packet())
+        return packets
 
-    def take_as_string(self, n: int) -> str:
+    def create_packets_with_total_length(self, length: int):
+        packets = []
+        while sum(packet.length for packet in packets) < length:
+            packets.append(self.create_packet())
+        return packets
+
+    def create_packet(self):
+        packet = Packet(version=self.take_as_int(3), type_=self.take_as_int(3))
+
+        if packet.type == 4:
+            last_group = False
+            literal_value_binary_string = ""
+            while not last_group:
+                if self.take_as_binary_string(1) == "0":
+                    last_group = True
+                literal_value_binary_string += self.take_as_binary_string(4)
+            packet.literal_value = int(literal_value_binary_string, 2)
+        else:
+            if self.take_as_binary_string(1) == "0":
+                total_length_of_subpackets = self.take_as_int(15)
+                packet.subpackets = self.create_packets_with_total_length(total_length_of_subpackets)
+        return packet
+
+    def take_as_binary_string(self, n: int) -> str:
         string = self.binary_string[:n]
         self.binary_string = self.binary_string[n:]
         return string
 
     def take_as_int(self, n: int) -> int:
-        return int(self.take_as_string(n), 2)
+        return int(self.take_as_binary_string(n), 2)
 
-
-
-
-
-
-print(Packet("8").binary_string)
+    def take_as_hexadecimal_string(self, n: int) -> str:
+        return format(self.take_as_int(n), "X")
--- aoc/year2021/day16/test_day16.py
+++ aoc/year2021/day16/test_day16.py
@@ -1,18 +1,23 @@
-from aoc.year2021.day16.day16 import Packet
+from aoc.year2021.day16.day16 import PacketFactory
 
 
 class TestPacket:
     def test_version(self):
-        packet = Packet('D2FE28')
+        packet = PacketFactory('D2FE28')
 
-        assert packet.version == 6
+        assert packet.create_packet().version == 6
 
     def test_type(self):
-        packet = Packet('D2FE28')
+        packet = PacketFactory('D2FE28')
 
-        assert packet.type == 4
+        assert packet.create_packet().type == 4
 
     def test_literal(self):
-        packet = Packet('D2FE28')
+        packet = PacketFactory('D2FE28')
 
-        assert packet.literal_value == 2021
+        assert packet.create_packet().literal_value == 2021
+
+    def test_operator_length_type_0(self):
+        packet = PacketFactory('38006F45291200')
+
+        assert len(packet.create_packet().subpackets) == 2
