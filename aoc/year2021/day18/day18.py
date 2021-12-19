e493583bdc0ac8a9385ce68862c9e9f71e8c4afc
Filip Lange <filip.lange@gmail.com>
2021-12-19 18:37:55 +0100

    2021 day18 explode working

--- aoc/year2021/day18/day18.py
+++ aoc/year2021/day18/day18.py
@@ -1,10 +1,11 @@
-from typing import List, Union
+from typing import List, Union, Optional
 from collections import deque
 
 
 class SnailfishNumber:
     def __init__(self, string: str):
         self.list = self._parse(deque(string))
+        self.locations = self._locations()
 
     def __repr__(self):
         return repr(self.list).replace(" ", "")
@@ -29,5 +30,39 @@ class SnailfishNumber:
             new_deque.append(source.popleft())
         return new_deque
 
-    def explode(self):
-        pass
+    def _locations(self):
+        return self._get_locations(self.list, [])
+
+    def __getitem__(self, location: List):
+        element = self.list
+        for i in location:
+            element = element[i]
+        return element
+
+    def __setitem__(self, location: List, value: Union[List, int]):
+        element = self.list
+        for i in location[:-1]:
+            element = element[i]
+        element[location[-1]] = value
+
+    @classmethod
+    def _get_locations(cls, list_: List, location_of_list: []):
+        locations = []
+        for current_location, element in enumerate(list_):
+            if isinstance(element, int):
+                locations.append(location_of_list + [current_location])
+            elif isinstance(element, list):
+                locations.extend(cls._get_locations(element, location_of_list + [current_location]))
+        return locations
+
+    def _explode(self):
+        shall_explode = [len(location) > 4 for location in self.locations]
+        explode_index = shall_explode.index(True)
+        explode_location = self.locations[explode_index]
+        left_value, right_value = self[explode_location[:-1]]
+
+        self[explode_location[:-1]] = 0
+        if explode_index < len(self.locations) - 2:  # -2 since explode_index is index of left digit
+            self[self.locations[explode_index + 2]] += right_value  # +2 since explode_index is index of left digit
+        if explode_index > 0:
+            self[self.locations[explode_index - 1]] += left_value
--- aoc/year2021/day18/test_day18.py
+++ aoc/year2021/day18/test_day18.py
@@ -15,11 +15,49 @@ class TestSnailfishNumber:
         assert SnailfishNumber._pop_until_enclosed_list_and_return(deque("[1,[2,3]]")) == deque("[1,[2,3]]")
 
     def test_parse(self):
-        assert SnailfishNumber._parse(deque("[1,2]")) == [1,2]
-        assert SnailfishNumber._parse(deque("[1,[2,3]]")) == [1,[2,3]]
+        assert SnailfishNumber._parse(deque("[1,2]")) == [1, 2]
+        assert SnailfishNumber._parse(deque("[1,[2,3]]")) == [1, [2, 3]]
 
-    @pytest.mark.xfail
-    def test_explode(self):
+    def test_indices(self):
+        assert SnailfishNumber("[1,2]")._locations() == [[0], [1]]
+        assert SnailfishNumber("[1,[2,3]]")._locations() == [[0], [1, 0], [1, 1]]
+
+    def test_indices_harder(self):
+        assert SnailfishNumber("[[1,2],[3,4]]")._locations() == [[0, 0], [0, 1], [1, 0], [1, 1]]
+
+    def test_getitem(self):
+        assert SnailfishNumber("[1,2]")[[0]] == 1
+
+    def test_setitem(self):
+        s = SnailfishNumber("[1,2]")
+        s[[0]] = 2
+        assert repr(s) == "[2,2]"
+
+    def test_setitem_nested(self):
+        s = SnailfishNumber("[1,[2,3]]")
+        s[[1, 1]] = 4
+        assert repr(s) == "[1,[2,4]]"
+
+    def test_getitem_nested(self):
+        assert SnailfishNumber("[[1,2],[3,4]]")[[0, 0]] == 1
+        assert SnailfishNumber("[[1,2],[3,4]]")[[1, 1]] == 4
+
+    def test_explode_leftmost(self):
         s = SnailfishNumber('[[[[[9,8],1],2],3],4]')
-        s.explode(0)
+        s._explode()
         assert str(s) == '[[[[0,9],2],3],4]'
+
+    def test_explode_rightmost(self):
+        s = SnailfishNumber('[7,[6,[5,[4,[3,2]]]]]')
+        s._explode()
+        assert str(s) == '[7,[6,[5,[7,0]]]]'
+
+    def test_explode_again(self):
+        s = SnailfishNumber('[[6,[5,[4,[3,2]]]],1]')
+        s._explode()
+        assert str(s) == '[[6,[5,[7,0]]],3]'
+
+    def test_explode_to_new_explosion_risk(self):
+        s = SnailfishNumber('[[3,[2,[1,[7,3]]]],[6,[5,[4,[3,2]]]]]')
+        s._explode()
+        assert str(s) == '[[3,[2,[8,0]]],[9,[5,[4,[3,2]]]]]'
