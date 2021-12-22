9df21d1dc628ef27f5898638bfa77f983ef92c0e
Filip Lange <filip.lange@gmail.com>
2021-12-22 14:39:39 +0100

    2021 day21 part2

--- aoc/year2021/day21/day21.py
+++ aoc/year2021/day21/day21.py
@@ -1,3 +1,6 @@
+import functools
+
+
 class Player:
     def __init__(self, position):
         self.position = position
@@ -43,3 +46,31 @@ def run(player1_position, player2_position) -> int:
 
 
 print(run(8, 2))
+
+STEPS_AND_COUNTS = ((3, 1), (4, 3), (5, 6), (6, 7), (7, 6), (8, 3), (9, 1))
+
+
+@functools.lru_cache(maxsize=None)
+def play(player1_position, player2_position, player1_score, player2_score):
+    if player1_score >= 21:
+        return 1, 0
+    elif player2_score >= 21:
+        return 0, 1
+
+    player1_wins = 0
+    player2_wins = 0
+
+    for steps, count in STEPS_AND_COUNTS:
+        new_player1_position = player1_position + steps
+        while new_player1_position > 10:
+            new_player1_position -= 10
+        new_player1_score = player1_score + new_player1_position
+        new_player2_wins, new_player1_wins = play(player2_position, new_player1_position, player2_score, new_player1_score)
+        player1_wins += new_player1_wins * count
+        player2_wins += new_player2_wins * count
+
+    return player1_wins, player2_wins
+
+
+print(max(play(8, 2, 0, 0)))
+# print(play(4, 8, 0, 0))
