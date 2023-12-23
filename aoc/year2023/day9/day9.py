be3914084227ed1759b96e46d6b06c29a0cf3ceb
Filip Lange <filip.lange@gmail.com>
2023-12-23 22:41:40 +0100

    2023 day 9 part 2

--- aoc/year2023/day9/day9.py
+++ aoc/year2023/day9/day9.py
@@ -1,28 +1,45 @@
+from collections import deque
 from aocd import data
 from aoc.utils import rows, ints
 import more_itertools
 
-histories = [ints(row) for row in rows(data)]
+histories = [deque(ints(row)) for row in rows(data)]
 
 
-def differences(sequence: list[int]):
-    return [b - a for a, b in more_itertools.pairwise(sequence)]
+def differences(sequence: deque[int]):
+    return deque(b - a for a, b in more_itertools.pairwise(sequence))
 
 
-def get_next_value(history: list[int]):
+def get_sequences_of_differences(history: deque[int]):
     sequences_of_differences = [history]
     while any(i != 0 for i in sequences_of_differences[-1]):
         sequences_of_differences.append(differences(sequences_of_differences[-1]))
-    print(sequences_of_differences)
     sequences_of_differences[-1].append(0)
+    return sequences_of_differences
+
+
+def get_next_value(history: deque[int]):
+    sequences_of_differences = get_sequences_of_differences(history)
     for i in range(len(sequences_of_differences) - 2, -1, -1):
         delta = sequences_of_differences[i + 1][-1]
         last_in_current_sequence = sequences_of_differences[i][-1]
         sequences_of_differences[i].append(last_in_current_sequence + delta)
-    print(sequences_of_differences)
     return sequences_of_differences[0][-1]
+
+
+def get_previous_value(history: deque[int]):
+    sequences_of_differences = get_sequences_of_differences(history)
+    for i in range(len(sequences_of_differences) - 2, -1, -1):
+        delta = sequences_of_differences[i + 1][0]
+        first_in_current_sequence = sequences_of_differences[i][0]
+        sequences_of_differences[i].appendleft(first_in_current_sequence - delta)
+    return sequences_of_differences[0][0]
 
 
 # print(get_next_value([0, 3, 6, 9, 12, 15]))
 
 print(sum(get_next_value(history) for history in histories))
+
+
+print(sum(get_previous_value(history) for history in histories))
+# print(get_previous_value(deque([0, 3, 6, 9, 12, 15])))
