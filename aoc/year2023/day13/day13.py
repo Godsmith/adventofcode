8cdc102f8e3ba8ace264038237160bd5bbbf90eb
Filip Lange <filip.lange@gmail.com>
2023-12-27 22:45:56 +0100

    2023 day13 part 2

--- aoc/year2023/day13/day13.py
+++ aoc/year2023/day13/day13.py
@@ -1,4 +1,5 @@
-from typing import Sequence
+import copy
+from typing import Iterable, Sequence
 from aocd import data
 import more_itertools
 
@@ -14,45 +15,56 @@ def check_reflection(rows: Sequence[Sequence[str]], index: int) -> bool:
     return True
 
 
-def find_reflection(rows: Sequence[Sequence[str]]) -> int:
+def find_reflections(rows: Sequence[Sequence[str]]) -> list[int]:
+    reflections = []
     for i, (row1, row2) in enumerate(more_itertools.pairwise(rows)):
         if row1 == row2:
             if check_reflection(rows, i):
-                return i
-    return -1
+                reflections.append(i)
+    return reflections
+
+
+def scores(pattern_string: str) -> list[int]:
+    rows: list[str] = pattern_string.split("\n")
+    scores_ = []
+    if row_indices := find_reflections(rows):
+        scores_.extend([100 * (row_index + 1) for row_index in row_indices])
+    columns = to_columns(rows)
+    if column_indices := find_reflections(columns):
+        scores_.extend([column_index + 1 for column_index in column_indices])
+    return scores_
 
 
 def score(pattern_string: str) -> int:
-    rows: list[str] = pattern_string.split("\n")
-    if (row_index := find_reflection(rows)) != -1:
-        return 100 * (row_index + 1)
-    columns = to_columns(rows)
-    column_index = find_reflection(columns)
-    return column_index + 1
+    return scores(pattern_string)[0]
 
 
 def to_columns(rows: Sequence[str]) -> list[list[str]]:
     return [[row[i] for row in rows] for i, _ in enumerate(rows[0])]
 
 
+def variants(pattern_string: str) -> Iterable[str]:
+    characters = list(pattern_string)
+    for i, char in enumerate(characters):
+        if char == "\n":
+            continue
+        new_characters = copy.deepcopy(characters)
+        if char == ".":
+            new_characters[i] = "#"
+        elif char == "#":
+            new_characters[i] = "."
+        yield "".join(new_characters)
+
+
+def variant_score(pattern_string: str):
+    original_score = score(pattern_string)
+    for variant in variants(pattern_string):
+        for score_ in scores(variant):
+            if score_ and score_ != original_score:
+                return score_
+    raise ValueError("no variant score found")
+
+
 pattern_strings = data.split("\n\n")
 print(sum(score(pattern_string) for pattern_string in pattern_strings))
-
-# test_pattern = """#.##..##.
-# ..#.##.#.
-# ##......#
-# ##......#
-# ..#.##.#.
-# ..##..##.
-# #.#.##.#."""
-
-# test_pattern_2 = """#...##..#
-# #....#..#
-# ..##..###
-# #####.##.
-# #####.##.
-# ..##..###
-# #....#..#"""
-
-# print(score(test_pattern))
-# print(score(test_pattern_2))
+print(sum(variant_score(pattern_string) for pattern_string in pattern_strings))
