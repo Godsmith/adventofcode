c4f901be61f1798e73e18affd113d53a7ea1c78b
Filip Lange <filip.lange@gmail.com>
2024-01-06 20:59:28 +0100

    2023 day 16 part 2

--- aoc/year2023/day16/day16.py
+++ aoc/year2023/day16/day16.py
@@ -28,6 +28,9 @@ class Beam:
     dx: int
     dy: int
 
+    def __repr__(self) -> str:
+        return f"Beam({x}, {y}, {dx}, {dy})"
+
     def move(self):
         self.x += self.dx
         self.y += self.dy
@@ -75,41 +78,63 @@ def print_energized(energized):
         print()
 
 
-energized = set()
-existing_beams = set()
-while beams:
-    energized.update((beam.x, beam.y) for beam in beams)
-    for beam in list(beams):
-        value = grid[beam.y][beam.x]
-        if value == "/":
-            beam.dx, beam.dy = beam.dy, beam.dx
-            beam.dx *= -1
-            beam.dy *= -1
-        elif value == "\\":
-            beam.dx, beam.dy = beam.dy, beam.dx
-        elif value == "-":
-            if beam.dy:
-                beam.dy = 0
-                beam.dx = -1
-                new_beam = dataclasses.replace(beam, dx=1)
-                beams.append(new_beam)
-        elif value == "|":
-            if beam.dx:
-                beam.dx = 0
-                beam.dy = -1
-                new_beam = dataclasses.replace(beam, dy=1)
-                beams.append(new_beam)
-        beam.move()
-    # energized.update((beam.x, beam.y) for beam in beams)
-    beams = [beam for beam in beams if beam.is_inside()]
-    beams = [
-        beam
-        for beam in beams
-        if (beam.x, beam.y, beam.dx, beam.dy) not in existing_beams
-    ]
-    # energized.update((beam.x, beam.y) for beam in beams)
-    existing_beams.update((beam.x, beam.y, beam.dx, beam.dy) for beam in beams)
+def get_energized_count(beams: list[Beam]):
+    energized = set()
+    existing_beams = set()
+    while beams:
+        energized.update((beam.x, beam.y) for beam in beams)
+        for beam in list(beams):
+            value = grid[beam.y][beam.x]
+            if value == "/":
+                beam.dx, beam.dy = beam.dy, beam.dx
+                beam.dx *= -1
+                beam.dy *= -1
+            elif value == "\\":
+                beam.dx, beam.dy = beam.dy, beam.dx
+            elif value == "-":
+                if beam.dy:
+                    beam.dy = 0
+                    beam.dx = -1
+                    new_beam = dataclasses.replace(beam, dx=1)
+                    beams.append(new_beam)
+            elif value == "|":
+                if beam.dx:
+                    beam.dx = 0
+                    beam.dy = -1
+                    new_beam = dataclasses.replace(beam, dy=1)
+                    beams.append(new_beam)
+            beam.move()
+        # energized.update((beam.x, beam.y) for beam in beams)
+        beams = [beam for beam in beams if beam.is_inside()]
+        beams = [
+            beam
+            for beam in beams
+            if (beam.x, beam.y, beam.dx, beam.dy) not in existing_beams
+        ]
+        # energized.update((beam.x, beam.y) for beam in beams)
+        existing_beams.update((beam.x, beam.y, beam.dx, beam.dy) for beam in beams)
+    return len(energized)
 
-print_energized(energized)
 
-print(len(energized))
+energized = get_energized_count(beams)
+
+print(energized)
+
+energized_counts = []
+for x in range(max_x + 1):
+    print(f"{x}/{max_x}")
+    for y in range(max_y + 1):
+        directions = []
+        if x == 0:
+            directions.append((1, 0))
+        if x == max_x:
+            directions.append((-1, 0))
+        if y == 0:
+            directions.append((0, 1))
+        if y == max_y:
+            directions.append((0, -1))
+        for dx, dy in directions:
+            beam = Beam(x, y, dx, dy)
+            print(repr(beam))
+            energized_counts.append(get_energized_count([beam]))
+print(max(energized_counts))
