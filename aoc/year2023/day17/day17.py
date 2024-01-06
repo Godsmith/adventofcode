c2ab6fbbed509014ae996c76fca09feb4aaee1fc
Filip Lange <filip.lange@gmail.com>
2024-01-06 22:55:09 +0100

    2023 day 17 part 2 not working yet

--- aoc/year2023/day17/day17.py
+++ aoc/year2023/day17/day17.py
@@ -4,19 +4,19 @@ from aocd import data
 from heapq import heappush, heappop
 
 
-# data = """2413432311323
-# 3215453535623
-# 3255245654254
-# 3446585845452
-# 4546657867536
-# 1438598798454
-# 4457876987766
-# 3637877979653
-# 4654967986887
-# 4564679986453
-# 1224686865563
-# 2546548887735
-# 4322674655533"""
+data = """2413432311323
+3215453535623
+3255245654254
+3446585845452
+4546657867536
+1438598798454
+4457876987766
+3637877979653
+4654967986887
+4564679986453
+1224686865563
+2546548887735
+4322674655533"""
 
 
 # Idea: spawn new crucibles all the time, collect the crucible properties
@@ -41,6 +41,19 @@ max_x = max(x for x, _ in grid)
 max_y = max(y for _, y in grid)
 
 
+def heat_loss(start_x, start_y, end_x, end_y):
+    heat_loss = 0
+    if start_y == end_y:
+        dx = (end_x - start_x) // abs(end_x - start_x)
+        for x in range(start_x + dx, end_x + dx, dx):
+            heat_loss += grid[(x, start_y)]
+    else:
+        dy = (end_y - start_y) // abs(end_y - start_y)
+        for y in range(start_y + dy, end_y + dy, dy):
+            heat_loss += grid[(start_x, y)]
+    return heat_loss
+
+
 @dataclass(frozen=True)
 class Crucible:
     x: int
@@ -49,30 +62,37 @@ class Crucible:
     heat_loss: int = 0
     blocks_since_last_turn: int = 0
     max_blocks_without_turning: int = 2
+    after_turn_minimum: int = 1
 
     def __lt__(self, other):
         return self.heat_loss < other.heat_loss
 
     def create_new_crucibles(self) -> list["Crucible"]:
         new_crucibles = []
-        directions_and_blocks_since_last_turn: list[tuple[tuple[int, int], int]] = [
-            (turn_left(*self.direction), 0),
-            (turn_right(*self.direction), 0),
+        directions_and_blocks_since_last_turn_and_minimum_move: list[
+            tuple[tuple[int, int], int, int]
+        ] = [
+            (turn_left(*self.direction), 0, self.after_turn_minimum),
+            (turn_right(*self.direction), 0, self.after_turn_minimum),
         ]
         if self.blocks_since_last_turn < self.max_blocks_without_turning:
-            directions_and_blocks_since_last_turn.append(
-                (self.direction, self.blocks_since_last_turn + 1)
+            directions_and_blocks_since_last_turn_and_minimum_move.append(
+                (self.direction, self.blocks_since_last_turn + 1, 1)
             )
-        for direction, blocks_since_last_turn in directions_and_blocks_since_last_turn:
-            x = self.x + direction[0]
-            y = self.y + direction[1]
+        for (
+            direction,
+            blocks_since_last_turn,
+            minimum_move,
+        ) in directions_and_blocks_since_last_turn_and_minimum_move:
+            x = self.x + direction[0] * minimum_move
+            y = self.y + direction[1] * minimum_move
             if 0 <= x <= max_x and 0 <= y <= max_y:
                 new_crucibles.append(
                     self.__class__(
                         x=x,
                         y=y,
                         direction=direction,
-                        heat_loss=self.heat_loss + grid[(x, y)],
+                        heat_loss=self.heat_loss + heat_loss(self.x, self.y, x, y),
                         blocks_since_last_turn=blocks_since_last_turn,
                         max_blocks_without_turning=self.max_blocks_without_turning,
                     )
@@ -83,9 +103,17 @@ class Crucible:
         return (self.x, self.y, self.direction, self.blocks_since_last_turn)
 
 
-def get_min_heat_loss():
+def get_min_heat_loss(max_blocks_without_turning, after_turn_minimum):
     crucibles = []
-    heappush(crucibles, Crucible(0, 0))
+    heappush(
+        crucibles,
+        Crucible(
+            0,
+            0,
+            max_blocks_without_turning=max_blocks_without_turning,
+            after_turn_minimum=after_turn_minimum,
+        ),
+    )
     previous_heat_losses = {}
 
     i = 0
@@ -108,4 +136,5 @@ def get_min_heat_loss():
                 heappush(crucibles, new_crucible)
 
 
-print(get_min_heat_loss())
+print(get_min_heat_loss(2, 1))
+print(get_min_heat_loss(9, 4))
