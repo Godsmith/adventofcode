b535cb47a3a167c96f5a2b5e7193b819997e3809
Filip Lange <filip.lange@gmail.com>
2024-01-10 22:26:29 +0100

    year 2023 day 18 part 1 more optimized

--- aoc/year2023/day18/day18.py
+++ aoc/year2023/day18/day18.py
@@ -1,74 +1,24 @@
-from collections import deque
 from dataclasses import dataclass
 from aocd import data
-from typing import Optional
 from aoc.utils import rows
 
-data = """R 6 (#70c710)
-D 5 (#0dc571)
-L 2 (#5713f0)
-D 2 (#d2c081)
-R 2 (#59c680)
-D 2 (#411b91)
-L 5 (#8ceee2)
-U 2 (#caa173)
-L 1 (#1b58a2)
-U 2 (#caa171)
-R 2 (#7807d2)
-U 3 (#a77fa3)
-L 2 (#015232)
-U 2 (#7a21e3)"""
+# data = """R 6 (#70c710)
+# D 5 (#0dc571)
+# L 2 (#5713f0)
+# D 2 (#d2c081)
+# R 2 (#59c680)
+# D 2 (#411b91)
+# L 5 (#8ceee2)
+# U 2 (#caa173)
+# L 1 (#1b58a2)
+# U 2 (#caa171)
+# R 2 (#7807d2)
+# U 3 (#a77fa3)
+# L 2 (#015232)
+# U 2 (#7a21e3)"""
 
 
 dxdy_from_direction = {"U": (0, -1), "D": (0, 1), "L": (-1, 0), "R": (1, 0)}
-
-x = 0
-y = 0
-trenches = {(0, 0)}
-for row in rows(data):
-    direction, steps, color = row.split(" ")
-    dx, dy = dxdy_from_direction[direction]
-    for i in range(int(steps)):
-        x += dx
-        y += dy
-        trenches.add((x, y))
-
-min_x = min(x for x, _ in trenches)
-min_y = min(y for _, y in trenches)
-max_x = max(x for x, _ in trenches)
-max_y = max(y for _, y in trenches)
-
-for y in range(min_y, max_y + 1):
-    for x in range(min_x, max_x + 1):
-        if (x, y) in trenches:
-            print("#", end="")
-        else:
-            print(".", end="")
-    print()
-
-edges = {(min_x - 1, min_y - 1)}
-visited = set()
-while edges:
-    x, y = edges.pop()
-    visited.add((x, y))
-    neighbors = ((x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1))
-    for x, y in neighbors:
-        if (x, y) in visited:
-            continue
-        if x < min_x - 1 or x > max_x + 1 or y < min_y - 1 or y > max_y + 1:
-            continue
-        if (x, y) in trenches:
-            continue
-        edges.add((x, y))
-
-# for y in range(min_y - 1, max_y + 2):
-#     for x in range(min_x - 1, max_x + 2):
-#         if (x, y) in visited:
-#             print("#", end="")
-#         else:
-#             print(".", end="")
-#     print()
-print((max_x - min_x + 3) * (max_y - min_y + 3) - len(visited))
 
 
 @dataclass
@@ -77,6 +27,7 @@ class Trench:
     y1: int
     x2: int
     y2: int
+    changes_inside_to_outside: bool = True
 
     def __post_init__(self):
         if self.x1 > self.x2:
@@ -84,11 +35,64 @@ class Trench:
         if self.y1 > self.y2:
             self.y1, self.y2 = self.y2, self.y1
 
-    def cross_section(self, y: int) -> Optional[tuple[int, int]]:
-        if self.y1 == self.y2:
-            return (self.x1, self.x2) if y == self.y1 else None
-        else:
-            return (self.x1, self.x1) if self.y1 <= y <= self.y2 else None
+    def has_overlap(self, y: int) -> bool:
+        return self.y1 <= y <= self.y2
+
+    def is_horizontal(self):
+        return self.x1 != self.x2
+
+    def set_changes_inside_to_outside(self, previous_trench, next_trench):
+        if self.is_horizontal():
+            is_previous_trench_above = (
+                min((previous_trench.y1, previous_trench.y2)) < self.y1
+            )
+            is_next_trench_above = min((next_trench.y1, next_trench.y2)) < self.y1
+            is_previous_trench_below = (
+                max((previous_trench.y1, previous_trench.y2)) > self.y1
+            )
+            is_next_trench_below = max((next_trench.y1, next_trench.y2)) > self.y1
+            self.changes_inside_to_outside = (
+                is_previous_trench_above and is_next_trench_below
+            ) or (is_previous_trench_below and is_next_trench_above)
+
+
+def print_area(trenches):
+    for i, trench in enumerate(trenches):
+        previous_trench = trenches[(i - 1) % len(trenches)]
+        next_trench = trenches[(i + 1) % len(trenches)]
+        trench.set_changes_inside_to_outside(previous_trench, next_trench)
+
+    ys = [trench.y1 for trench in trenches] + [trench.y2 for trench in trenches]
+    min_y = min(ys)
+    max_y = max(ys)
+
+    area = 0
+    for y in range(min_y, max_y + 1):
+        overlapping_trenches = sorted(
+            [trench for trench in trenches if trench.has_overlap(y)],
+            key=lambda trench: trench.x1,
+        )
+
+        is_inside = False
+        x_start = 0
+        # print(overlapping_trenches)
+        for trench in overlapping_trenches:
+            if trench.changes_inside_to_outside:
+                if not is_inside:
+                    x_start = trench.x1
+                else:
+                    darea = trench.x2 - x_start + 1
+                    area += darea
+                    # print(darea)
+                is_inside = not is_inside
+            else:
+                if is_inside:
+                    continue
+                else:
+                    darea = trench.x2 - trench.x1 + 1
+                    area += darea
+                    # print(darea)
+    print(area)
 
 
 trenches = []
@@ -109,39 +113,4 @@ for row in rows(data):
     x += dx
     y += dy
 
-xs = [trench.x1 for trench in trenches] + [trench.x2 for trench in trenches]
-ys = [trench.y1 for trench in trenches] + [trench.y2 for trench in trenches]
-min_x = min(xs)
-min_y = min(ys)
-max_x = max(xs)
-max_y = max(ys)
-
-area = 0
-for y in range(min_y, max_y + 1):
-    cross_sections = [trench.cross_section(y) for trench in trenches]
-    cross_sections = [
-        cross_section for cross_section in cross_sections if cross_section
-    ]
-    cross_sections.sort()
-
-    is_inside = False
-    x_start = 0
-    print(cross_sections)
-    for x1, x2 in cross_sections:
-        if x1 != x2:
-            if is_inside:
-                continue
-            else:
-                darea = x2 - x1 + 1
-                print(darea)
-                area += darea
-        else:
-            if not is_inside:
-                is_inside = True
-                x_start = x1
-            else:
-                is_inside = False
-                darea = x1 - x_start + 1
-                area += darea
-                print(darea)
-print(area)
+print_area(trenches)
