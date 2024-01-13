f035e347002f130e4b719bbfe33531499e6f6df7
Filip Lange <filip.lange@gmail.com>
2024-01-13 19:59:45 +0100

    2023 day 19 part 2

--- aoc/year2023/day19/day19.py
+++ aoc/year2023/day19/day19.py
@@ -1,4 +1,6 @@
-from dataclasses import dataclass
+import copy
+from dataclasses import dataclass, field
+from itertools import chain
 import re
 import operator
 from typing import Any, Callable
@@ -27,13 +29,15 @@ Operator = Callable[[Any, Any], bool]
 
 Part = namedtuple("Part", ["x", "m", "a", "s"])
 
+accept_all_operator = lambda x, y: True
 
-@dataclass
+
+@dataclass(frozen=True)
 class Rule:
-    category: str
-    operator: Operator
-    value: int
     destination: str
+    category: str = "x"
+    operator: Operator = accept_all_operator
+    value: int = 0
 
     def applies_to(self, part: Part):
         return self.operator(getattr(part, self.category), self.value)
@@ -41,14 +45,13 @@ class Rule:
 
 @dataclass
 class Workflow:
+    name: str
     rules: list[Rule]
-    default_destination: str
 
     def get_destination(self, part: Part) -> str:
         for rule in self.rules:
             if rule.applies_to(part):
                 return rule.destination
-        return self.default_destination
 
 
 workflows_chunk, parts_chunk = data.split("\n\n")
@@ -64,8 +67,9 @@ for row in workflow_rows:
         operator_ = operator.gt if ">" in rule_string else operator.lt
         condition_string, destination = rule_string.split(":")
         category, value_string = re.split("[<>]", condition_string)
-        rules.append(Rule(category, operator_, int(value_string), destination))
-    workflows[name] = Workflow(rules, default_destination)
+        rules.append(Rule(destination, category, operator_, int(value_string)))
+    rules.append(Rule(default_destination))
+    workflows[name] = Workflow(name, rules)
 
 parts_rows = parts_chunk.splitlines()
 parts = [Part(*map(int, re.findall(r"\d+", row))) for row in parts_rows]
@@ -82,3 +86,68 @@ def get_destination(part: Part):
 accepted_parts = [part for part in parts if get_destination(part) == "A"]
 
 print(sum(sum(part) for part in accepted_parts))
+
+
+# Part 2
+# Key finding: every workflow only ever sends to one place
+
+
+def rating_range() -> list[int]:
+    return list(range(1, 4001))
+
+
+@dataclass
+class PartRatingRange:
+    x: list[int] = field(default_factory=rating_range)
+    m: list[int] = field(default_factory=rating_range)
+    a: list[int] = field(default_factory=rating_range)
+    s: list[int] = field(default_factory=rating_range)
+
+    def __repr__(self):
+        return f"PartRatingRange(x={len(self.x)},m={len(self.m)},a={len(self.a)},s={len(self.s)})"
+
+    @property
+    def combinations(self):
+        return len(self.x) * len(self.m) * len(self.a) * len(self.s)
+
+
+def comply_with_rule(range_: PartRatingRange, rule: Rule):
+    list_ = getattr(range_, rule.category)
+    new_range = copy.deepcopy(range_)
+    setattr(
+        new_range,
+        rule.category,
+        [value for value in list_ if rule.operator(value, rule.value)],
+    )
+    return new_range
+
+
+def skip_rule(range_: PartRatingRange, rule: Rule):
+    list_ = getattr(range_, rule.category)
+    new_range = copy.deepcopy(range_)
+    setattr(
+        new_range,
+        rule.category,
+        [value for value in list_ if not rule.operator(value, rule.value)],
+    )
+    return new_range
+
+
+accepted_ranges = []
+ranges_and_workflows: list[tuple[PartRatingRange, Workflow]] = [
+    (PartRatingRange(), workflows["in"])
+]
+while ranges_and_workflows:
+    prr, workflow = ranges_and_workflows.pop()
+    for rule in workflow.rules:
+        new_prr = comply_with_rule(prr, rule)
+        if rule.destination == "A":
+            accepted_ranges.append(new_prr)
+        elif rule.destination == "R":
+            pass
+        else:
+            ranges_and_workflows.append((new_prr, workflows[rule.destination]))
+        prr = skip_rule(prr, rule)
+
+print(accepted_ranges)
+print(sum(range_.combinations for range_ in accepted_ranges))
