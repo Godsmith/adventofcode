72fa02dc82e300961a1b8e690a42cf444911bda2
Filip Lange <filip.lange@ericsson.com>
2025-12-23 09:43:57 +0100

    Refactoring

--- aoc/year2025/day1/day1.py
+++ aoc/year2025/day1/day1.py
@@ -1,28 +1,41 @@
 from aocd import data
 
-value = 50
-count = 0
 
-# data = """L68
-# L30
-# R48
-# L5
-# R60
-# L55
-# L1
-# L99
-# R14
-# L82"""
+def main():
+    value = 50
+    count = 0
 
-rotations = data.splitlines()
+    # data = """L68
+    # L30
+    # R48
+    # L5
+    # R60
+    # L55
+    # L1
+    # L99
+    # R14
+    # L82"""
 
-for rotation in rotations:
-    direction = 1 if rotation.startswith("R") else -1
-    steps = int(rotation[1:])
+    rotations = data.splitlines()
+
+    for rotation in rotations:
+        direction = 1 if rotation.startswith("R") else -1
+        steps = int(rotation[1:])
+        count += zero_count(direction, steps, value)
+        value += direction * steps
+        value %= 100
+    print(count)
+
+
+def zero_count(direction, steps, value):
+    count = 0
     for _ in range(steps):
         value += direction
         value %= 100
         if value == 0:
             count += 1
-    print(value)
-print(count)
+    return count
+
+
+if __name__ == "__main__":
+    main()
